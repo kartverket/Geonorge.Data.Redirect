21063f44e4b133f6978cdc477edb33f8c6efaa8e
veronika@arkitektum.no <veronika@arkitektum.no>
2015-07-15 14:11:16 +0200

    Get service url from namespace register

--- data.redirect/Controllers/HomeController.cs
+++ data.redirect/Controllers/HomeController.cs
@@ -44,12 +44,34 @@ namespace data.redirect.Controllers
             //http://data.geonorge.no/so/naturbase/naturvernområder/62883310 
             //http://data.geonorge.no/naturbase/so/naturvernområder/62883310 (IKKE LOV)= http://wfs.miljodirektoratet.no/arcgis/services/vern/mapserver/WfSServer?&Service=wfs&version=1.1.1&request=getfeature&gml_ID=62883310
 
-            //Slå opp i namespace register med domene + register + /so/ for å finne tjeneste som skal redirectes til
+            //Slå opp i namespace register med domene + /so/ + register for å finne tjeneste som skal redirectes til
+
+            string redirectServiceUrl = GetServiceUrlFromNamespaceRegister(@namepace);
+            return Redirect(redirectServiceUrl);
+
             //Løse redirect url
             //gml_ID=lokalitet.35553312 = {register}/so/{objekttype}.{lokalid}
             //gml_ID=35553312 = {register}/so/{lokalid} og {register}/so/{objekttype}/{lokalid}
-            return Redirect("http://wfs.fiskeridirektoratet.no/arcgis/services/akvakultur/mapserver/WfSServer?&Service=wfs&version=1.1.1&request=getfeature&gml_ID=" + lokalid);
+            //return Redirect("http://wfs.fiskeridirektoratet.no/arcgis/services/akvakultur/mapserver/WfSServer?&Service=wfs&version=1.1.1&request=getfeature&gml_ID=" + localId);
            
+        }
+
+        public string GetServiceUrlFromNamespaceRegister(string @namespace) {
+
+            System.Net.WebClient c = new System.Net.WebClient();
+            c.Encoding = System.Text.Encoding.UTF8;
+            var data = c.DownloadString("http://register.dev.geonorge.no/api/register/navnerom/");
+            var response = Newtonsoft.Json.Linq.JObject.Parse(data);
+            var namespases = response["containeditems"];
+
+            foreach (var item in namespases)
+            {
+                if (item["label"].ToString().Contains(@namespace))
+                {
+                    return item["serviceUrl"].ToString();
+                }
+            }            
+            return null;
         }
     }
 }
